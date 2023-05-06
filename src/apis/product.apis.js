24bcee5b6b39e973168e325d84edbc2310905117
mmr-ashiq <mizanur35-2844@diu.edu.bd>
2023-05-06 23:11:11 +0600

    product apiendpoint route added

--- src/apis/product.apis.js
+++ src/apis/product.apis.js
@@ -1,8 +1,16 @@
-import { API_CLIENT } from "./axios";
+import { API_CLIENT } from './axios';
 
-export const getProductsApi = async () => API_CLIENT.get("/products");
+export const getProductsApi = async () => API_CLIENT.get('/products');
 export const getSingleProductApi = async (id) =>
-  API_CLIENT.get(`/products/${id}/single`);
+	API_CLIENT.get(`/products/${id}/single`);
 
 export const updateProductApi = async ({ id, data }) =>
-  API_CLIENT.put(`/products/${id}`, data);
+	API_CLIENT.put(`/products/${id}`, data);
+
+export const getTopSixProductsApi = async (month) => {
+	const monthData = month ? `?month=${month}` : '';
+
+	return API_CLIENT.get(`/top-six-products${monthData}`);
+};
+
+export const yearlySellReportApi = async () => API_CLIENT.get(`/sell-report`);
