eebec2996e43309c28ae0a8a5335173948398b0d
sadabdhrubo1997 <sadabdhrubo@gmail.com>
2023-07-09 20:58:23 +0600

    dashboard chart

--- src/components/Dashboard/Pie2.jsx
+++ src/components/Dashboard/Pie2.jsx
@@ -1,46 +1,62 @@
 import { Chart } from 'chart.js';
-import { useEffect, useRef } from 'react';
+import { useEffect, useState, useRef } from 'react';
+import { topCustomerApi } from './../../apis/customer.apis';
 
 export const Pie2 = () => {
-	const chartRef = useRef(null);
-	let chartInstance = null;
+  const [customersData, setCustomerData] = useState([]);
+  const chartRef = useRef(null);
+  let chartInstance = null;
+  let _customersData = [];
 
-	useEffect(() => {
-		const chartCanvas = chartRef.current.getContext('2d');
-		const data = {
-			labels: [],
-			datasets: [
-				{
-					data: [12, 19, 3, 5, 2, 3],
-					backgroundColor: [
-						'#FF6384',
-						'#36A2EB',
-						'#FFCE56',
-						'#00FF00',
-						'#800080',
-						'#FFA500',
-					],
-				},
-			],
-		};
+  async function getTopCustomer() {
+    const {
+      data: {
+        data: { customers },
+      },
+    } = await topCustomerApi();
+    if (customers.length) {
+      console.log('customers = ', customers);
+      for (let i = 0; i < customers.length; i++) {
+        const element = customers[i]?.quantity;
+        console.log('element ', element);
+        _customersData.push(element);
+      }
+      setCustomerData(_customersData);
+    }
+    console.log('hjkl - ', customersData);
+  }
 
-		if (chartInstance) {
-			// Destroy the existing chart instance
-			chartInstance.destroy();
-		}
+  useEffect(() => {
+    getTopCustomer();
+    const chartCanvas = chartRef.current.getContext('2d');
+    const data = {
+      labels: [],
+      datasets: [
+        {
+          data: customersData || [5, 10, 15],
+          //   data: [12, 19, 3, 5, 2, 3, 15, 16, 200],
+          backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#00FF00', '#800080', '#FFA500'],
+        },
+      ],
+    };
 
-		// Create a new chart instance
-		chartInstance = new Chart(chartCanvas, {
-			type: 'doughnut', // Set chart type to 'doughnut'
-			data: data,
-			options: {
-				cutoutPercentage: 50,
-			},
-		});
-	}, []);
-	return (
-		<>
-			<canvas ref={chartRef} />
-		</>
-	);
+    if (chartInstance) {
+      // Destroy the existing chart instance
+      chartInstance.destroy();
+    }
+
+    // Create a new chart instance
+    chartInstance = new Chart(chartCanvas, {
+      type: 'doughnut', // Set chart type to 'doughnut'
+      data: data,
+      options: {
+        cutoutPercentage: 50,
+      },
+    });
+  }, []);
+  return (
+    <>
+      <canvas ref={chartRef} />
+    </>
+  );
 };
