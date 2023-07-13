479cca87cdc15ca4fad0b456bde8b4ce2df09d89
abid <abidshahriar7@gmail.com>
2023-07-13 21:51:24 +0600

    fixes

--- src/modules/product/ProductDetailsModal.jsx
+++ src/modules/product/ProductDetailsModal.jsx
@@ -5,7 +5,7 @@ export default function ProductDetailsModal({ productId }) {
     'https://source.unsplash.com/random/400x200/?nature',
     'https://source.unsplash.com/random/400x200/?technology',
     'https://source.unsplash.com/random/400x200/?fashion',
-    'https://source.unsplash.com/random/400x200/?food',
+    'https://source.unsplash.com/random/400x200/?food'
   ];
   const randomIndex = Math.floor(Math.random() * randomImages.length);
   const randomImage = randomImages[randomIndex];
@@ -13,34 +13,42 @@ export default function ProductDetailsModal({ productId }) {
   const { isLoading, data } = useGetSingleProduct(productId);
 
   const { product } = data || {};
+
   return (
     <>
       {isLoading && 'Loading'}
       <>
         {typeof product !== 'undefined' && (
-          <div className="p-4">
-            <h1 className="mb-4 text-2xl font-bold">Product Details</h1>
-            <img src={randomImage} className="w-64 h-auto mb-8 rounded-sm" alt="Product" />
-            <p className="mb-4">
-              <span className="font-bold">Product Name:</span> {product?.name}
+          <div className='p-4'>
+            <h1 className='mb-4 text-2xl font-bold'>Product Details</h1>
+            <img
+              src={product?.images?.[0] || randomImage}
+              className='w-64 h-auto mb-8 rounded-sm'
+              alt='Product'
+            />
+            <p className='mb-4'>
+              <span className='font-bold'>Product Name:</span> {product?.name}
             </p>
-            <p className="mb-4">
-              <span className="font-bold">Product Description:</span> {product?.description}
+            <p className='mb-4'>
+              <span className='font-bold'>Product Description:</span>{' '}
+              {product?.description}
             </p>
-            <p className="mb-4">
-              <span className="font-bold">Category:</span> <Category data={product?.category} />
+            <p className='mb-4'>
+              <span className='font-bold'>Category:</span>{' '}
+              <Category data={product?.category} />
             </p>
-            <p className="mb-4">
-              <span className="font-bold">Price:</span> {product?.price}
+            <p className='mb-4'>
+              <span className='font-bold'>Price:</span> {product?.price}
             </p>
-            <p className="mb-4">
-              <span className="font-bold">Variants:</span> <Category data={product?.variants} />
+            <p className='mb-4'>
+              <span className='font-bold'>Variants:</span>{' '}
+              <Category data={product?.variants} />
             </p>
-            <p className="mb-4">
-              <span className="font-bold">Discount:</span> {product?.discount}
+            <p className='mb-4'>
+              <span className='font-bold'>Discount:</span> {product?.discount}
             </p>
-            <p className="mb-4">
-              <span className="font-bold">Stock:</span> {product?.stock}
+            <p className='mb-4'>
+              <span className='font-bold'>Stock:</span> {product?.stock}
             </p>
           </div>
         )}
